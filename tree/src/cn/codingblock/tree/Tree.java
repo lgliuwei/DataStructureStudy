8c968e6939ecb8ecc6c5889578c9901e576ecb2b
CodingBlock <lg_liuwei@163.com>
2017-08-17 23:11:11 +0800

    1、二叉树

--- tree/src/cn/codingblock/tree/Tree.java
+++ tree/src/cn/codingblock/tree/Tree.java
@@ -1,5 +1,7 @@
 package cn.codingblock.tree;
 
+import cn.codingblock.utils.log.Logger;
+
 /**
  * Created by liuwei on 17/8/16.
  */
@@ -38,13 +40,90 @@ public class Tree {
         }
     }
 
-    public TreeNode deleteFirst(int e) {
-        return null;
-    }
+    public TreeNode delete(int e) {
+        TreeNode currentNode = root;
+        TreeNode pNode = null;
+        boolean isLeftChild = false;
+        while (currentNode != null && currentNode.val != e) {
+            pNode = currentNode;
+            if (e < currentNode.val) {
+                isLeftChild = true;
+                currentNode = currentNode.leftChild;
+            } else {
+                isLeftChild = false;
+                currentNode = currentNode.rightChid;
+            }
+        }
 
-    public int deleteAll(int e) {
-        int count = 0;
-        return count;
+        if (currentNode == null) {
+            return null;
+        } else if (currentNode.leftChild != null && currentNode.rightChid != null) {
+            // 1、待删除的节点有两个子节点
+            /*
+             * 对于这种情况
+             * ① 首先找出待删除节点的后继节点(后继节点为中序遍历中的下一个节点)
+             * ② 用后继节点替换掉待删除节点,并删除后继节点
+             * ③ 如果后继节点还有子节点,将其子节点上移替代后继节点的位置。(由①知,后继节点最多存在一个右节点(或右子树))
+             */
+            TreeNode nextNode = currentNode.rightChid; // 后继节点
+            TreeNode parentNextNode = null; // 后继节点的父节点
+            while (nextNode.leftChild != null) {
+                parentNextNode = nextNode;
+                nextNode = nextNode.leftChild;
+            }
+
+            currentNode.val = nextNode.val; // 将当前节点的值赋值为后继节点
+            if (parentNextNode == null) {
+                if (nextNode.rightChid != null) {
+                    currentNode.rightChid = nextNode.rightChid;
+                } else {
+                    currentNode.rightChid = null;
+                }
+            } else {
+                if (nextNode.rightChid != null) {
+                    parentNextNode.leftChild = nextNode.rightChid; // 若后继节点存在右节点,将其父节点的leftChild指向后继节点的右节点即可
+                } else {
+                    parentNextNode.leftChild = null; // 后继节点如果没有子节点,将其父节点的指向置为null,让其等待垃圾回收即可
+                }
+            }
+
+        } else if (currentNode.leftChild != null || currentNode.rightChid != null) {
+            // 2、待删除的节点只有一个子节点
+            if (currentNode == root) {
+                if (currentNode.leftChild != null) {
+                    root = root.leftChild;
+                } else {
+                    root = root.rightChid;
+                }
+            } else {
+                // 2.1 将currentNode 赋值为下一个子节点(左节点或右节点)
+                if (currentNode.leftChild != null) {
+                    currentNode = currentNode.leftChild;
+                } else {
+                    currentNode = currentNode.rightChid;
+                }
+                // 2.2 将父节点的指向更新
+                if (isLeftChild) {
+                    pNode.leftChild = currentNode;
+                } else {
+                    pNode.rightChid = currentNode;
+                }
+            }
+        } else {
+            if (currentNode == root) {
+                // 整棵树只有 root 一个节点,待删除是 root
+                root = null;
+            } else {
+                // 待删除的节点没有子节点
+                if (isLeftChild) { // 待删除的节点为父节点的左节点
+                    pNode.leftChild = null;
+                } else {
+                    pNode.rightChid = null;
+                }
+            }
+        }
+
+        return null;
     }
 
     public TreeNode find(int e) {
@@ -59,5 +138,81 @@ public class Tree {
             }
         }
         return null;
+    }
+
+    public TreeNode min() {
+        TreeNode currentNode = root;
+        while (currentNode != null && currentNode.leftChild != null) {
+            currentNode = currentNode.leftChild;
+        }
+        return currentNode;
+    }
+
+    public TreeNode max() {
+        TreeNode currentNode = root;
+        while (currentNode != null && currentNode.rightChid != null) {
+            currentNode = currentNode.rightChid;
+        }
+        return currentNode;
+    }
+
+    public void inOrder() {
+        realInOrder(root);
+    }
+
+    /**
+     * 中序遍历
+     *  1、先调用自身遍历节点的左子树
+     *  2、访问节点
+     *  3、再调用自身遍历节点的右子数
+     * @param treeNode
+     */
+    private void realInOrder(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        realInOrder(treeNode.leftChild);
+        Logger.print(treeNode.val + ", ");
+        realInOrder(treeNode.rightChid);
+    }
+
+    public void preOrder() {
+        realPreOrder(root);
+    }
+
+    /**
+     * 前序遍历
+     *  1、访问节点
+     *  2、调用自身遍历节点的左子树
+     *  3、调用自身遍历节点的右子树
+     * @param treeNode
+     */
+    private void realPreOrder(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        Logger.print(treeNode.val + ", ");
+        realPreOrder(treeNode.leftChild);
+        realPreOrder(treeNode.rightChid);
+    }
+
+    public void postOrder() {
+        realPostOrder(root);
+    }
+
+    /**
+     * 后序遍历
+     *  1、调用自身遍历节点的左子树
+     *  2、调用自身遍历节点的右子树
+     *  3、访问节点
+     * @param treeNode
+     */
+    private void realPostOrder(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        realPostOrder(treeNode.leftChild);
+        realPostOrder(treeNode.rightChid);
+        Logger.print(treeNode.val + ", ");
     }
 }
--- tree/src/cn/codingblock/tree/TreeTest.java
+++ tree/src/cn/codingblock/tree/TreeTest.java
@@ -1,19 +1,39 @@
 package cn.codingblock.tree;
 
 import cn.codingblock.utils.log.Logger;
+import cn.codingblock.utils.random.Random;
 
 /**
  * Created by liuwei on 17/8/16.
  */
 public class TreeTest {
     public static void main(String[] strs) {
+        int[] nums = Random.initArrayByRandomNoRepeat(20);
         Tree tree = new Tree();
-        tree.insert(5);
-        tree.insert(8);
-        tree.insert(2);
-        tree.insert(0);
-        tree.insert(9);
-
-        Logger.println("find:" + tree.find(10).val);
+//        tree.insert(5);
+//        tree.insert(8);
+//        tree.insert(2);
+//        tree.insert(0);
+//        tree.insert(9);
+//        tree.insert(3);
+//        tree.insert(6);
+//        tree.insert(7);
+//        tree.insert(1);
+//        tree.insert(4);
+        for (int n:
+             nums) {
+            tree.insert(n);
+        }
+        Logger.println("1、中序遍历:");
+        tree.inOrder();
+        tree.delete(nums[5]);
+        Logger.println("\n删除" + nums[5] + "后:");
+        tree.inOrder();
+//        Logger.println("\n2、前序遍历:");
+//        tree.preOrder();
+//        Logger.println("\n3、后序遍历:");
+//        tree.postOrder();
+//        Logger.println("\n最小值:" + tree.min().val);
+//        Logger.println("\n最大值:" + tree.max().val);
     }
 }
